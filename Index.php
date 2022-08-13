15837ff3d3191030d9b96ad80b8b8e296054b704
Saify Abid Bhuiyan <80751001+Saify007@users.noreply.github.com>
2022-08-13 23:52:10 +0600

    Slot two
    Final Upload

---
+++ Index.php
@@ -0,0 +1,228 @@
+
+<?php include('Controller/navbar.php');?>
+ 
+ <!DOCTYPE html>
+ <html>
+ <head>
+ <style> 
+#myDIV {
+  padding-top: 5em;
+  position: absolute;
+  width: 880px;
+  height: 480px;
+  background-color: rgba(255, 255, 255, .15);  
+  backdrop-filter: blur(5px);
+  box-shadow: 10px 20px 30px lightblue;
+  color: white;
+  left: 28%;
+  
+}
+
+h1{
+  font-family: 'Lobster Two', cursive;
+  font-family: 'Orbitron', sans-serif;
+  font-size: 68px;
+
+}
+
+h2{
+  font-family: 'Lobster Two', cursive;
+  font-family: 'Orbitron', sans-serif;
+  font-size: 28px;
+}
+
+
+</style>
+</head>
+<body>
+
+
+
+<div id="myDIV">
+  <h2> Welcome To</h2><br>
+  
+  <h1>Vehicle Rental System</h1><br>
+ 
+  <h2>Rent vehicle within a short time </h2><br>
+</div>
+
+
+
+
+ 	<title>Home</title>
+ 	<style>
+ 	
+ 		footer {
+   
+  			padding: 60px;
+  			background: rgb(2,0,36);
+        background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+  			color: white;
+  			height: 100%;
+  			width: 100%;
+}
+.main {
+	align-items: center;
+}
+@media only screen and (max-width:800px) {
+  /* For tablets: */
+  .main {
+    width: 80%;
+    padding: 0;
+  }
+ }
+ @media only screen and (max-width:300px) {
+  /* For mobile phones: */
+  .main {
+    width: 100%;
+  }
+}
+
+
+body{
+
+  width: 100%;
+  height: 100%;
+  color: white;
+
+  background: rgb(2,0,36);
+  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+  margin: auto;
+  height: 50%;
+  width: 100%;
+  position: sticky;
+  top: 20%;
+  left: 25%;
+ 
+
+}
+h1{
+  left: 25%;
+  padding: auto;
+}
+
+
+ 	</style>
+ </head>
+ <body>
+ 	<div class="main">
+ 	
+  </div>
+
+<style>
+
+
+@import url('https://fonts.googleapis.com/css2?family=Lobster+Two:ital@1&family=Orbitron:wght@800&display=swap');
+
+.text-box{
+  color: trasparent;
+  position: absolute;
+  bottom: 8%;
+}
+.text-box p{
+  font-size: 50px;
+  font-weight: 500;
+}
+.text-box h1{
+  top: 40%;
+  left: 40%;
+  text-align: right;
+  font-family: 'Lobster Two', cursive;
+  font-family: 'Orbitron', sans-serif;
+  font-size: 68px;
+
+
+}
+.text-box h2{
+
+  left: 40%;
+  top: 40%;
+  text-align: right;
+  font-size: 48px;
+  font-family: 'Lobster Two', cursive;
+  font-family: 'Orbitron', sans-serif;
+
+}
+
+<div class=container4>
+  <p>Centered!
+</div>
+
+
+</style>
+
+	
+	<style>
+
+
+  DIV.container {
+    min-height: 10em;
+    display: table-cell;
+    vertical-align: middle }
+...
+<DIV class="container">
+  <P>This small paragraph...
+</DIV>
+
+
+
+
+.centerText {
+  text-align: center; /* This puts the text into the center of the 
+  screen. */
+}
+
+
+
+
+
+
+
+
+
+		
+/* Slideshow container */
+.slideshow-container {
+  max-width: 650px;
+  position: relative;
+  margin: auto;
+  width: 80%;
+  background: rgb(2,0,36);
+  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  margin-bottom: 25px;
+}
+
+
+.active {
+background: rgb(2,0,36);
+background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+}
+
+
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .text {font-size: 11px}
+}
+
+/* footer section styling */
+footer{
+	background: rgb(2,0,36);
+  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+  padding:  0 50px 0 100px;
+  text-align: center;
+  display: absolute;
+}
+footer span a{
+  text-decoration: none;
+}
+footer span a:hover{
+  text-decoration: underline;
+}
+	</style>
+ 	<footer>
+ 		<?php include('footer.php');?>
+</footer>
+ </body>
+ </html>
